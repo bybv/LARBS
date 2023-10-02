9129dcf78e18479d2d5b52828b68d1bfdb4907df
bybv <143136502+bybv@users.noreply.github.com>
2023-10-02 01:46:38 +0000

    Update and rename larbs.sh to larbs-bybv.sh
    Modified URLs to reflect my files.

--- static/larbs-bybv.sh
+++ static/larbs-bybv.sh
@@ -6,8 +6,8 @@
 
 ### OPTIONS AND VARIABLES ###
 
-dotfilesrepo="https://github.com/lukesmithxyz/voidrice.git"
-progsfile="https://raw.githubusercontent.com/LukeSmithxyz/LARBS/master/static/progs.csv"
+dotfilesrepo="https://github.com/bybv/voidrice.git"
+progsfile="https://raw.githubusercontent.com/bybv/LARBS/master/static/progs.csv"
 aurhelper="yay"
 repobranch="master"
 export TERM=ansi
